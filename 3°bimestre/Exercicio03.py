b861a66403c89ed1964f98db53314071a5b36986
Laura Alves Figueiredo <lauraalvesfigueiredo2468@gmail.com>
2026-09-11 11:17:52 -0300

    Create Exercicio03.py

---
+++ 3°bimestre/Exercicio03.py
@@ -0,0 +1,11 @@
+n = int(input('Digite um numero: '))
+contador = 0
+if n > 0:
+    print('Seu numero é positivo, Vamos contar até ele:')
+    while contador <= n:
+            print (f"{contador}")
+            contador +=1
+   
+
+else:
+    (' seu nemero não é positivo, tente outro.')
