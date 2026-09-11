f524a74c634e8133f2a177ddad833471ffec50cc
Laura Alves Figueiredo <lauraalvesfigueiredo2468@gmail.com>
2026-09-11 11:17:06 -0300

    Create Exercicio02.py

---
+++ 3°bimestre/Exercicio02.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um numero: '))
+n2 = int(input('Digite um segeundo nemero:'))
+if n > n2:
+    print(f'O maior numero é: {n}')
+elif n2 > n:
+    print(f'O maior numero é: {n2}')
+else:
+    print(f'os numeros são iguais')
